ef9a5af26851fc6129e02728f31096737737d010
Ben Barnard <barnardb@gmail.com>
2016-02-04 00:31:03 +0100

    Clarify DeferredInvocationTest

--- src/test/scala/io/github/barnardb/scalainvoke/DeferredInvocationTest.scala
+++ src/test/scala/io/github/barnardb/scalainvoke/DeferredInvocationTest.scala
@@ -4,34 +4,32 @@ import org.scalatest.FunSuite
 
 class DeferredInvocationTest extends FunSuite {
 
-  class Box(init: String) {
+  class Var(init: String) {
     var value = init
   }
-  val strategy = new FunctionLifter[Map[String, Box], Function0]
-    with ImplicitArgumentExtractors[Map[String, Box]]
+
+  val strategy = new FunctionLifter[Map[String, Var], Function0]
+    with ImplicitArgumentExtractors[Map[String, Var]]
     with DeferredInvocation
 
-  implicit object BoxExtractor extends Extractor[Map[String, Box], String] {
-    override def extract(a: Map[String, Box], name: String): String = {
+  implicit object VarExtractor extends Extractor[Map[String, Var], String] {
+    override def extract(a: Map[String, Var], name: String): String = {
       println(s"extracting $name from $a")
       a(name).value
     }
   }
 
-  test("lifted method application extracts parameters immediate but defers execution") {
-    var stage = "init"
+  test("lifted method application extracts parameters immediately but defers execution") {
+    var stage = "lifting stage"
+    val lifted = strategy.lift((boxed: String) => s"param extracted during $boxed, evaluated during $stage")
 
-    val lifted = strategy.lift((boxed: String) => s"param: $boxed, exec: $stage")
-
-    stage = "eager"
-    val box = new Box("eager")
-
+    stage = "primary invocation stage"
+    val box = new Var("primary invocation")
     val deferred: () => String = lifted(Map("boxed" -> box))
 
-    stage = "deferred"
-    box.value = "deferred"
-
-    assertResult("param: eager, exec: deferred") {
+    stage = "deferred invocation stage"
+    box.value = "deferred invocation"
+    assertResult("param extracted during primary invocation, evaluated during deferred invocation stage") {
       deferred()
     }
   }
