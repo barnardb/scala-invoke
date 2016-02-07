4fa6655cfb1a4626bc8d731bcd2603b0c7ba6e98
Ben Barnard <barnardb@gmail.com>
2016-02-08 00:34:13 +0100

    Try to make the example more explanatory

--- src/main/scala/io/github/barnardb/scalainvoke/Lift.scala
+++ src/main/scala/io/github/barnardb/scalainvoke/Lift.scala
@@ -30,45 +30,70 @@ import io.github.barnardb.scalainvoke.macroutil.OwnerChainCorrector
   * implicit val invocationStrategy = new DirectInvocation
   * }}}
   *
+  * @example
+  *
   * Here's an example that gives an idea of how to use [[Lift]].
   *
   * {{{
-  * object Example {
-  *   import io.github.barnardb.scalainvoke._
+  * object `your application's domain` {
   *
-  *   final class Path(override val toString: String) extends AnyVal
   *   case class HttpRequest(path: Path, query: Map[String, String])
   *
-  *   implicit val valueExtractionStrategy = new ImplicitlyDiscoveredValueExtractors[HttpRequest]
-  *   implicit val invocationStrategy = new DeferredInvocation
+  *   final class Path(override val toString: String) extends AnyVal {
+  *     def group: String = toString.stripPrefix("/").split("/").head
+  *   }
+  *
+  *   def action(path: Path, material: String, weight: Double): (String, Double) =
+  *     "Do something with " + path.group + " and " + material -> weight * 2
+  * }
+  *
+  * object `extractors for use with scalainvoke` {
+  *   import `your application's domain`._
+  *   import io.github.barnardb.scalainvoke.{NamedValueExtractor, ValueExtractor}
+  *
+  *   implicit object StringExtractor extends NamedValueExtractor[HttpRequest, String] {
+  *     override def extract(request: HttpRequest, name: String): String = request.query(name)
+  *   }
+  *
+  *   implicit object DoubleExtractor extends NamedValueExtractor[HttpRequest, Double] {
+  *     override def extract(request: HttpRequest, name: String): Double = request.query(name).toDouble
+  *   }
   *
   *   implicit object PathExtractor extends ValueExtractor[HttpRequest, Path] {
   *     override def extract(request: HttpRequest): Path = request.path
   *     override def extract(request: HttpRequest, name: String): Path = request.path
   *   }
-  *   implicit object StringExtractor extends NamedValueExtractor[HttpRequest, String] {
-  *     override def extract(request: HttpRequest, name: String): String = request.query(name)
-  *   }
-  *   implicit object DoubleExtractor extends NamedValueExtractor[HttpRequest, Double] {
-  *     override def extract(request: HttpRequest, name: String): Double = request.query(name).toDouble
-  *   }
+  * }
   *
-  *   def action(path: Path, material: String, weight: Double): (String, Double) =
-  *     "Do something with " + path.group + " and " + material -> weight * 2
+  * object `lifting` {
+  *   import `your application's domain`._
+  *   import `extractors for use with scalainvoke`._
+  *   import io.github.barnardb.scalainvoke._
   *
-  *   // This should happen once, somewhere in the setup of the program
-  *   private[this] final val liftedAction = Lift.function(action _)
+  *   // Choose which strategies you want to use
+  *   implicit val valueExtractionStrategy = new ImplicitlyDiscoveredValueExtractors[HttpRequest]
+  *   implicit val invocationStrategy = new DeferredInvocation
+  *
+  *   // Lift things
+  *   val liftedAction = Lift.function(action _)
+  * }
+  *
+  * object `calling the lifted function` extends App {
+  *   import `your application's domain`._
+  *   import `lifting`.liftedAction
   *
   *   // Because we are using DeferredExecution, we have a 2-step process to invoke.
   *
-  *   // Passing the environment to the lifted action causes the parameters to be extracted and an invoker to be returned.
-  *   val invoker: () => (String, Double) =
-  *     liftedAction(HttpRequest(new Path("/metal/bars"), Map("material" -> "gold", "weight" -> "42.1337")))
-  *
+  *   // Passing an environment to the lifted action causes the parameters to be extracted and an invoker to be returned.
+  *   val environment = HttpRequest(new Path("/metal/bars"), Map("material" -> "gold", "weight" -> "42.1337"))
+  *   val invoker: () => (String, Double) = liftedAction(environment)
   *   // At this point we know we have successfully extracted the arguments, but haven't yet invoked the function.
   *
   *   // We can invoke the function when we're ready.
   *   val (message: String, doubledWeight: Double) = invoker()
+  *
+  *   println(message)
+  *   println(doubledWeight)
   * }
   * }}}
   */
--- src/test/scala/io/github/barnardb/scalainvoke/example/Example.scala
+++ src/test/scala/io/github/barnardb/scalainvoke/example/Example.scala
@@ -1,39 +1,57 @@
 package io.github.barnardb.scalainvoke.example
 
-object Example extends App {
-  import io.github.barnardb.scalainvoke._
+object `your application's domain` {
+
+  case class HttpRequest(path: Path, query: Map[String, String])
 
   final class Path(override val toString: String) extends AnyVal {
     def group: String = toString.stripPrefix("/").split("/").head
   }
-  case class HttpRequest(path: Path, query: Map[String, String])
 
-  implicit val valueExtractionStrategy = new ImplicitlyDiscoveredValueExtractors[HttpRequest]
-  implicit val invocationStrategy = new DeferredInvocation
+  def action(path: Path, material: String, weight: Double): (String, Double) =
+    "Do something with " + path.group + " and " + material -> weight * 2
+}
+
+object `extractors for use with scalainvoke` {
+  import `your application's domain`._
+  import io.github.barnardb.scalainvoke.{NamedValueExtractor, ValueExtractor}
+
+  implicit object StringExtractor extends NamedValueExtractor[HttpRequest, String] {
+    override def extract(request: HttpRequest, name: String): String = request.query(name)
+  }
+
+  implicit object DoubleExtractor extends NamedValueExtractor[HttpRequest, Double] {
+    override def extract(request: HttpRequest, name: String): Double = request.query(name).toDouble
+  }
 
   implicit object PathExtractor extends ValueExtractor[HttpRequest, Path] {
     override def extract(request: HttpRequest): Path = request.path
     override def extract(request: HttpRequest, name: String): Path = request.path
   }
-  implicit object StringExtractor extends NamedValueExtractor[HttpRequest, String] {
-    override def extract(request: HttpRequest, name: String): String = request.query(name)
-  }
-  implicit object DoubleExtractor extends NamedValueExtractor[HttpRequest, Double] {
-    override def extract(request: HttpRequest, name: String): Double = request.query(name).toDouble
-  }
+}
 
-  def action(path: Path, material: String, weight: Double): (String, Double) =
-    "Do something with " + path.group + " and " + material -> weight * 2
+object `lifting` {
+  import `your application's domain`._
+  import `extractors for use with scalainvoke`._
+  import io.github.barnardb.scalainvoke._
 
-  // This should happen once, somewhere in the setup of the program
-  private[this] final val liftedAction = Lift.function(action _)
+  // Choose which strategies you want to use
+  implicit val valueExtractionStrategy = new ImplicitlyDiscoveredValueExtractors[HttpRequest]
+  implicit val invocationStrategy = new DeferredInvocation
+
+  // Lift things
+  val liftedAction = Lift.function(action _)
+}
+
+object `calling the lifted function` extends App {
+  import `your application's domain`._
+  import `lifting`.liftedAction
 
   // Because we are using DeferredExecution, we have a 2-step process to invoke.
 
-  // Passing the environment to the lifted action causes the parameters to be extracted and an invoker to be returned.
-  val invoker: () => (String, Double) =
-    liftedAction(HttpRequest(new Path("/metal/bars"), Map("material" -> "gold", "weight" -> "42.1337")))
-
+  // Passing an environment to the lifted action causes the parameters to be extracted and an invoker to be returned.
+  val environment = HttpRequest(new Path("/metal/bars"), Map("material" -> "gold", "weight" -> "42.1337"))
+  val invoker: () => (String, Double) = liftedAction(environment)
   // At this point we know we have successfully extracted the arguments, but haven't yet invoked the function.
 
   // We can invoke the function when we're ready.
